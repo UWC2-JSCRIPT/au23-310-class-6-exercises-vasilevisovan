3702f4b1928f03505f4fedf3aa00a07138380538
vasilevisovan <vasile.visovan@yahoo.com>
2023-11-20 08:21:51 -0800

    #2

--- carProto.js
+++ carProto.js
@@ -5,59 +5,67 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
-class Car {
-    constructor(model){
-    this.model = model;
-    this.speed = 0;
-    }
-    // Method to accelerate the car
-    accelerate() {
-        // Generate a random speed between 1 and 50 (for example)
-        const randomSpeed = Math.floor(Math.random() * 50) + 1;
-        this.speed += randomSpeed;
-        console.log(`${this.model} is accelerating. Current speed: ${this.speed} km/h`);
-      }
-  
-    // Method to brake the car
-    brake() {
-      if (this.speed > 0) {
-        this.speed -= 5;
-        console.log(`${this.model} is braking. Current speed: ${this.speed} km/h`);
-      } else {
-        console.log(`${this.model} is already stopped.`);
-      }
-    };
-  
-    // Method to display car information
-    toString() {
-      return `${this.model} - Current speed: ${this.speed} km/h`;
-    };
-  }
-  
-  // Create an instance of the Car class
-  const myCarDaciaDuster = new Car("Dacia Duster");
-  
-  // Accelerate twice
-  myCarDaciaDuster.accelerate();
-  myCarDaciaDuster.accelerate();
-  
-  // Brake once
-  myCarDaciaDuster.brake();
-  
-  // Console log the instance toString()
-  console.log(myCarDaciaDuster.toString());
-
 /**
  * ElectricCar class
  * @constructor
  * @param {String} model
  */
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
-const myCarRivianR1T = new Car("Rivian R1T");
 
-myCarRivianR1T.accelerate();
-myCarRivianR1T.accelerate();
+class Car {
+  constructor(model) {
+      this.model = model;
+      this.speed = 0;
+  }
 
-myCarRivianR1T.brake();
+  accelerate() {
+      const randomSpeed = Math.floor(Math.random() * 50) + 1;
+      this.speed += randomSpeed;
+      console.log(`${this.model} is accelerating. Current speed: ${this.speed} km/h`);
+  }
 
-console.log(myCarRivianR1T.toString());
+  brake() {
+      if (this.speed > 0) {
+          this.speed -= 5;
+          console.log(`${this.model} is braking.`);
+      } else {
+          console.log(`${this.model} is already stopped.`);
+      }
+  }
+
+  toString() {
+      return `${this.model} - Current speed: ${this.speed} km/h`;
+  }
+}
+
+class ElectricCar extends Car {
+  constructor(model) {
+      super(model);
+      this.motor = "electric";
+  }
+
+  accelerate() {
+      // Increase the speed by twice the random speed
+      const randomSpeed = Math.floor(Math.random() * 50) + 1;
+      this.speed += 2 * randomSpeed;
+      console.log(`${this.model} (Electric) is accelerating. Current speed: ${this.speed} km/h`);
+  }
+
+  toString() {
+      return `${this.model} (Electric) - Current speed: ${this.speed} km/h`;
+  }
+}
+
+const myCarDaciaDuster = new Car("Dacia Duster");
+
+myCarDaciaDuster.accelerate();
+myCarDaciaDuster.accelerate();
+myCarDaciaDuster.brake();
+console.log(myCarDaciaDuster.toString());
+
+const myElectricCar = new ElectricCar("Tesla Model S");
+
+myElectricCar.accelerate();
+myElectricCar.accelerate();
+myElectricCar.brake();
+console.log(myElectricCar.toString());

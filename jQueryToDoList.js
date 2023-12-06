2fc7ea01b02255c2a5bc34a04ef89b67b03c0202
vasilevisovan <vasile.visovan@yahoo.com>
2023-12-06 09:26:36 -0800

    #4

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,78 +1,40 @@
-/**
- * Toggles "done" class on <li> element
- */
 
-/**
- * Delete element when delete link clicked
-/**
- * Adds new list item to <ul>
- */
-/// add listener for add
-
-$(document).ready(function() {
+$(document).ready(function () {
   // Add click event listener to the "Delete" links
-  $('.delete').on('click', function() {
+  $('.today-list').on('click', '.delete', function () {
     // Toggle the "done" class on the parent <li> element
     $(this).parent('li').toggleClass('done');
+    
+    // Remove the parent <li> element
+    $(this).parent('li').remove();
   });
-});
 
-  // Delete element when delete link is clicked
-  $(document).ready(function() {
-    // Add click event listener to the "Delete" links
-    $('.delete').on('click', function() {
-      // Remove the parent <li> element
-      $(this).parent('li').remove();
-    });
-  });
-  
   // Add new list item to <ul> on button click
-  $(document).ready(function() {
-    // Add click event listener to the "Add" button
-    $('.add-item').on('click', function() {
-      // Get the value from the input field
-      var newItemText = $('#new-todo').val();
-  
-      // Check if the input is not empty
-      if (newItemText.trim() !== '') {
-        // Create a new list item with the input value and a "Delete" link
-        var newListItem = $('<li><span>' + newItemText + '</span><a class="delete">Delete</a></li>');
-  
-        // Append the new list item to the <ul>
-        $('.today-list').append(newListItem);
-  
-        // Clear the input field
-        $('#new-todo').val('');
-      }
-    });
+  $('.add-item').on('click', function () {
+    addItem();
   });
-  
-  $(document).ready(function() {
-    // Add click event listener to the "Add" button
-    $('.add-item').on('click', addItem);
-  
-    // Add keypress event listener for the "Enter" key
-    $('#new-todo').keypress(function(event) {
-      if (event.which === 13) {
-        addItem();
-      }
-    });
-  
-    function addItem() {
-      // Get the value from the input field
-      var newItemText = $('#new-todo').val();
-  
-      // Check if the input is not empty
-      if (newItemText.trim() !== '') {
-        // Create a new list item with the input value and a "Delete" link
-        var newListItem = $('<li><span>' + newItemText + '</span><a class="delete">Delete</a></li>');
-  
-        // Append the new list item to the <ul>
-        $('.today-list').append(newListItem);
-  
-        // Clear the input field
-        $('#new-todo').val('');
-      }
+
+  // Add keypress event listener for the "Enter" key
+  $('#new-todo').keypress(function (event) {
+    if (event.which === 13) {
+      addItem();
     }
   });
-  
+
+  function addItem() {
+    // Get the value from the input field
+    var newItemText = $('#new-todo').val();
+
+    // Check if the input is not empty
+    if (newItemText.trim() !== '') {
+      // Create a new list item with the input value and a "Delete" link
+      var newListItem = $('<li><span>' + newItemText + '</span><a class="delete">Delete</a></li>');
+
+      // Append the new list item to the <ul>
+      $('.today-list').append(newListItem);
+
+      // Clear the input field
+      $('#new-todo').val('');
+    }
+  }
+});
